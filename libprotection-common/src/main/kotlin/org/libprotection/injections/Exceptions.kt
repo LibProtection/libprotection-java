45c48e19ef7d0b3f6593a005ff94c384d6ecad77
Igor Yakovlev <iyakovlev@ptsecurity.com>
2018-04-27 18:35:02 +0300

    updated gradle scripts

--- libprotection-common/src/main/kotlin/org/libprotection/injections/Exceptions.kt
+++ libprotection-common/src/main/kotlin/org/libprotection/injections/Exceptions.kt
@@ -2,4 +2,4 @@ package org.libprotection.injections
 
 class AttackDetectedException : Throwable()
 
-class LanguageNotSupportedException(message : String)
+class LanguageNotSupportedException(val message : String)
