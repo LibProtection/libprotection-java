0c328a5c342b953e960cae7da2e089e849e4ccf7
Igor Yakovlev <iyakovlev@ptsecurity.com>
2018-05-04 16:39:18 +0300

    added sqlite provider + refactoring

--- testSite-js/routes/index.js
+++ testSite-js/routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const sqlite = require("sqlite3");
 
 const LP = require("../lib/libprotection/index.js")
 
@@ -20,10 +22,11 @@ function FormatResult(successfully, formattedValue){
 	}
 }
 
-function Result(formatResult, operationResult) {
+function Result(formatResult, operationResult, exception) {
 	return {
 		get FormatResult() { return formatResult; },
-		get OperationResult() { return operationResult; }
+		get OperationResult() { return operationResult; },
+		get Exception() { return exception; }
 	}
 }
 
@@ -33,9 +36,52 @@ const examples = {
 		LP.format.bind(null, LP.Html),
 		"<a href='{0}' onclick='alert(\"{1}\");return false'>{2}</a>",
 		"Default.aspx\r\nHello from embedded JavaScript code!\r\nThis site's home page",
-		s => s
-	)
+		s => Promise.resolve(s)
+	),
+	JavaScript: Example(
+		"Executes the given JavaScript code on the client side.",
+        LP.format.bind(null, LP.JavaScript),
+        "operationResult.innerText = '{0}';",
+        "Hello from internal JavaScript code!",
+        s => Promise.resolve("<script>\r\n" + s + "\r\n</script>")
+	),
+	Sql: Example(
+		"Executes the given SQL query on the sever side and outputs its results.",
+        LP.format.bind(null, LP.Sql),
+        "SELECT * FROM myTable WHERE id = {0} AND myColumn = '{1}'",
+        "1\r\nvalue1",
+        sqlRequestTagBuilder)
+	
 };
+
+//	"start": "node --inspect-brk ./bin/www"
+function sqlRequestTagBuilder(request) {
+	
+	if (!request) { return ""; }
+
+	const db = new sqlite.Database(path.resolve("assets", "Database.sqlite"), sqlite.OPEN_READONLY, (openErr) => {
+		if(openErr !== null) return Promise.reject(openErr);
+	});
+	
+	return new Promise((resolve, reject) => {
+
+		db.all(request, (err, rows) => {
+			if(err !== null || rows === undefined || rows.forEach === undefined) { reject(err); return; }
+			
+			let result = "";
+			
+			rows.forEach( row =>  {
+				result = result + "Id: " + row.Id + ", myColumn: \'" + row.myColumn + "\'<br>";
+			})
+			
+			db.close();
+			
+			resolve(result);
+		});
+	});
+}
+
+
 
 function getLowerCasedProperty(obj, key){
 	
@@ -51,34 +97,55 @@ function getLowerCasedProperty(obj, key){
 	return undefined;
 }
 
-function getResultsFor(example, format, parameters) {
-	let formatResult = example.FormatFunc(
-		format,
-		... parameters.replace("\r\n", "\n").replace("\r", "\n").split("\n")
-	);
+async function getResultsFor(example, format, parameters) {
+	
+	let result;
+	try{
+		let formatResult = example.FormatFunc(
+			format,
+			... parameters.replace("\r\n", "\n").replace("\r", "\n").split("\n")
+		);
 
-	return Result(formatResult, example.TagBuilder(formatResult));
+		result = Result(formatResult, await example.TagBuilder(formatResult), null);
+	}catch(e){
+		result = Result(null, null, e);
+	}
+	return result;
 }
 
-function rootHandler(req, res, next) {
-  const currentExample = examples[getLowerCasedProperty(req.query, "Id")];
-  res.render('index',
-  { 
-	Title: 'LibProtection Test Page', 
-	Examples: examples,
-	get Id() { 
-		let id = getLowerCasedProperty(req.query, "id");
-		return id ? id : Object.keys(examples)[0];
-	},
-	get Parameters() { return getLowerCasedProperty(req.query, "Parameters"); },
-	get Format() { return getLowerCasedProperty(req.query, "Format"); },
-	get InputsAreDisabled() { return false; },
-	get ShowDisclaimer() {
-		res.cookie("showDisclaimer", "false");
-		return (req.cookies.showDisclaimer === undefined || req.cookies.showDisclaimer != "false");
-	},
-	getResultsFor: getResultsFor
-  });
+async function rootHandler(req, res) {
+
+	const model = {};
+
+	model.showDisclaimer = (req.cookies.showDisclaimer === undefined || req.cookies.showDisclaimer !== "false");
+	res.cookie("showDisclaimer", "false");
+
+	model.ids = Object.keys(examples);
+	
+	model.id = getLowerCasedProperty(req.query, "id");
+	model.id = model.id ? model.id : model.ids[0];
+
+	const currentExample = examples[model.id] ;
+	
+	if(currentExample === undefined){
+		res.render('index', model);
+		return;
+	}
+
+	model.exampleOperation = currentExample.Operation;
+	model.inputsAreDisabled = false;
+	
+	model.parameters = getLowerCasedProperty(req.query, "Parameters");
+	model.parameters = model.parameters ? model.parameters : currentExample.Parameters;
+
+	
+
+	model.format = getLowerCasedProperty(req.query, "Format");
+	model.format = (model.format && !model.inputsAreDisabled) ? model.format : currentExample.Format;
+
+	model.result = await getResultsFor(currentExample, model.format, model.parameters);
+	
+	res.render('index', model);
 }
 
 
